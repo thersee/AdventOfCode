99a8a6b5aaa52a97540930ebd7f4681b589167ca
thersee <theresea@axis.com>
2025-12-12 14:34:29 +0100

    better solution

--- 2025/12/12a.py
+++ 2025/12/12a.py
@@ -15,23 +15,33 @@ while i < 6*5:
                 volume += 1
     shapes.append(volume)
     i += 5
-print(shapes)
+
 
 possible = 0
+totalLines = 0
+impossible = 0
 for k in range(i,len(lines)):
+    totalLines += 1
     line = lines[k]
-    print(line)
     rect, values = line.split(': ')
     values = values.split(' ')
     sides = rect.split('x')
+    a = int(int(sides[0])/3)
+    b = int(int(sides[1])/3)
+    tot = 0
+    for v in values:
+        tot += int(v)
+    if a*b >= tot:
+        possible += 1
+
     totalArea = int(sides[0])*int(sides[1])
     area = 0
     for ind, v in enumerate(values):
         area += int(v)*shapes[ind]
-    print(area, totalArea)
-    if area < totalArea:
-        possible += 1
 
-print(possible)
+    if area > totalArea:
+        impossible += 1
 
+print(possible, totalLines, impossible)
 
+#472
